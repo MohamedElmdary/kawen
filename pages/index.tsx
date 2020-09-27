261b15f10a9eba7bd66100d32962e2d0bcb10c4c
MohamedElmdary <engm5081@gmail.com>
2020-09-27 19:47:22 +0200

    Added Assets & Apply Layout to Landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,24 +6,27 @@ const LandingPage: React.FC = () => {
     const [value, setValue] = useState('');
     return (
         <Layout>
-            <div>
-                <h1>init commit</h1>
-            </div>
-            <p className="h5 h5-regular">should have h5 styles</p>
-            <div style={{ margin: '50px', padding: '20px' }}>
-                <InputControl
-                    label="Full Name"
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                />
-            </div>
-            <div style={{ margin: '50px', padding: '20px' }}>
-                <InputControl
-                    label="Full Name"
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                />
-            </div>
+            <h1>h1</h1>
+            <h2>h2</h2>
+            <h3>h3</h3>
+            <h4>h4</h4>
+            <h5>h5</h5>
+            <h5 className="h5-regular">h5 regular</h5>
+            <p>body</p>
+            <InputControl
+                {...{ value }}
+                onChange={(e) => setValue(e.target.value)}
+                label="Full Name"
+            />
+            <p className="button">button style</p>
+            <button className="btn">regular Button</button>
+            <br />
+            <button className="btn btn-round">rounded button</button>
+            <br />
+            <button className="btn btn-round btn-outline">
+                outlined rounded button
+            </button>
+            <br />
         </Layout>
     );
 };
