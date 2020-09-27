79897cccffa2dd1120945f694c1ac21d068d6d76
MohamedElmdary <engm5081@gmail.com>
2020-09-27 15:26:57 +0200

    Added InputControl Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../components/Layout';
+import InputControl from '../components/inputControl';
 
 const LandingPage: React.FC = () => {
+    const [value, setValue] = useState('');
     return (
         <Layout>
             <div>
                 <h1>init commit</h1>
             </div>
             <p className="h5 h5-regular">should have h5 styles</p>
+            <div style={{ margin: '50px' }}>
+                <InputControl
+                    label="Full Name"
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                />
+            </div>
         </Layout>
     );
 };
