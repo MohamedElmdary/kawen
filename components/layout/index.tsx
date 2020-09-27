d6afb45ef00df6f5685423b585f97bdb24cf7d93
MohamedElmdary <engm5081@gmail.com>
2020-09-27 19:47:05 +0200

    Added Layout Component

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import Head from 'next/head';
+import Navbar from '../navbar';
+import Footer from '../footer';
 import classes from './layout.module.scss';
 
-const Layout: React.FC = (props) => {
-    const { children } = props;
-    return <>{children}</>;
+interface Meta {
+    name: string;
+    content: string;
+}
+
+interface Props {
+    logoOnly?: boolean;
+    title?: string;
+    metas?: Meta[];
+}
+
+const Layout: React.FC<Props> = (props) => {
+    const { children, logoOnly = false, title = '', metas = [] } = props;
+
+    const metaTags = metas.map((meta) => {
+        return <meta key={meta.name} {...meta} />;
+    });
+
+    return (
+        <section className={classes.app_container}>
+            <Head>
+                <title>{title}</title>
+                {metaTags}
+            </Head>
+            <Navbar {...{ logoOnly }} />
+            <main>{children}</main>
+            {!logoOnly && <Footer />}
+        </section>
+    );
 };
 
 export default Layout;
