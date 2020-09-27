a9f5cdb11c590a778209e4182cb7e42ec5943f7b
MohamedElmdary <engm5081@gmail.com>
2020-09-27 13:39:24 +0200

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const LandingPage: React.FC = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>init commit</h1>
+            </div>
+        </Layout>
+    );
+};
+
+export default LandingPage;
