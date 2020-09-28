bd0285f2b43fcfe75cc6adbb761da5a710bc2d05
Nahla Galal <nahlaglal@gmail.com>
2020-09-28 16:24:48 +0200

    Change path for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,79 @@
-import React, { useState } from 'react';
-import Layout from '../components/layout';
-import InputControl from '../components/inputControl';
+import React from "react";
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import Layout from "../components/layout";
+import classes from "./Home.module.scss";
 
-const LandingPage: React.FC = () => {
-    const [value, setValue] = useState('');
-    return (
-        <Layout title="Kawen | LandingPage">
-            <h1>h1</h1>
-            <h2>h2</h2>
-            <h3>h3</h3>
-            <h4>h4</h4>
-            <h5>h5</h5>
-            <h5 className="h5-regular">h5 regular</h5>
-            <p>body</p>
-            <InputControl
-                {...{ value }}
-                onChange={(e: any) => setValue(e.target.value)}
-                label="Full Name"
-            />
-            <p className="button">button style</p>
-            <button className="btn">regular Button</button>
-            <br />
-            <button className="btn btn-round">rounded button</button>
-            <br />
-            <button className="btn btn-round btn-outline">
-                outlined rounded button
-            </button>
-            <br />
-        </Layout>
-    );
+interface Field {
+	title: string;
+	description: string;
+	image: string;
+}
+
+type Props = { data: Field[] };
+
+const data: Field[] = [
+	{
+		title: "Front End development",
+		description: "Lorem",
+		image: "/images/home__fieldImg.png",
+	},
+	{
+		title: "Android development",
+		description:
+		"Lorem ipsum dolor sit amet, sectetur adipiscing elit, sed do ei us mod tempor incididunt ut lab ore et",
+		image: "/images/home__fieldImg.png",
+	},
+	{
+		title: "Back End development",
+		description:
+		"Lorem ipsum dolor sit amet, sectetur adipiscing elit, sed do ei us mod tempor incididunt ut lab ore et",
+		image: "/images/home__fieldImg.png",
+	},
+	{
+		title: "Security",
+		description:
+		"Lorem ipsum dolor sit amet, sectetur adipiscing elit, sed do ei us mod tempor incididunt ut lab ore et",
+		image: "/images/home__fieldImg.png",
+	},
+	{
+		title: "Data science",
+		description:
+		"Lorem ipsum dolor sit amet, sectetur adipiscing elit, sed do ei us mod tempor incididunt ut lab ore et Lorem ipsum dolor sit amet, sectetur adipiscing elit, sed do ei us mod tempor incididunt ut lab ore et",
+		image: "/images/home__fieldImg.png",
+	},
+	{
+		title: "IOS development",
+		description:
+		"Lorem ipsum dolor sit amet, sectetur adipiscing elit, sed do ei us mod tempor incididunt ut lab ore et",
+		image: "/images/home__fieldImg.png",
+	},
+];
+
+const Home: React.FC<Props> = ({ data }) => {
+	return (
+		<Layout title="Kawen">
+			<section className={`${classes.Home} container`}>
+				<h1 className="h4">Explore our paths to find your perfect program</h1>
+				{data.map((field: Field) => (
+					<section key={field.title}>
+						<img src={field.image} alt={`${field.title} image`} />
+						<h2>{field.title}</h2>
+						<p>{field.description}</p>
+						<Link href={`/${field.title.split(" ").join("-")}`}>
+							<button className="btn btn-round btn-outline">Explore</button>
+						</Link>
+					</section>
+				))}
+			</section>
+		</Layout>
+	);
 };
 
-export default LandingPage;
+export const getServerSideProps: GetServerSideProps = async () => {
+	return {
+		props: { data },
+	};
+};
+
+export default Home;
