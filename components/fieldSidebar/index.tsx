315687a97ecefd178b72d7ef1055b4c908b79dd2
Nahla Galal <nahlaglal@gmail.com>
2020-09-29 23:12:28 +0200

    Edit link to styles

--- components/fieldSidebar/index.tsx
+++ components/fieldSidebar/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import classes from "./fieldSidBar.module.scss";
+import classes from "./fieldSidbar.module.scss";
 
 export interface IFieldSidebar {
     field: string;
