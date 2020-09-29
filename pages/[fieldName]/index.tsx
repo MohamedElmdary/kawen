46db36e429060a9724daa44fe7ba8068adedbe25
MohamedElmdary <engm5081@gmail.com>
2020-09-30 01:54:53 +0200

    hotfix: fixed un-used variable issue in build

--- pages/[fieldName]/index.tsx
+++ pages/[fieldName]/index.tsx
@@ -1,38 +1,43 @@
-import React from "react";
-import { GetServerSideProps } from "next";
-import Link from "next/link";
-import Layout from "../../components/layout";
-import classes from "./fieldOverview.module.scss";
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+import classes from './fieldOverview.module.scss';
 
 interface Props {
     field: string;
     description: string;
     field_overview: string;
-    learning_path: {[key: string]: string[]};
+    learning_path: { [key: string]: string[] };
 }
 
 const data: Props = {
-    field: "Front end development",
+    field: 'Front end development',
     description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua',
     field_overview:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magn aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate veli esse cillum dolore eu fugiat nulla pariatur. Excepteur sin occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magn aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate veli esse cillum dolore eu fugiat nulla pariatur. Excepteur sin occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     learning_path: {
         Entry: [
-            "Introduction to HTML",
-            "CSS - the completed course",
-            "CSS3 - Including flexbox, transition, and animation",
+            'Introduction to HTML',
+            'CSS - the completed course',
+            'CSS3 - Including flexbox, transition, and animation',
         ],
         Intermediate: [
-            "Javascript syntax",
-            "Javascript dom",
-            "Javascript events",
+            'Javascript syntax',
+            'Javascript dom',
+            'Javascript events',
         ],
-        Master: ["React.js", "Redux", "Redux saga"],
+        Master: ['React.js', 'Redux', 'Redux saga'],
     },
 };
 
-const FieldOverview: React.FC<Props> = ({ field, description, field_overview, learning_path }) => {
+const FieldOverview: React.FC<Props> = ({
+    field,
+    description,
+    field_overview,
+    learning_path,
+}) => {
     return (
         <Layout title={`${field} overview`}>
             <header className={`${classes.field_header}`}>
@@ -79,7 +84,7 @@ const FieldOverview: React.FC<Props> = ({ field, description, field_overview, le
     );
 };
 
-export const getServerSideProps: GetServerSideProps = async (ctx: any) => {
+export const getServerSideProps: GetServerSideProps = async (/* ctx: any */) => {
     // const { fieldName } = ctx.query;
 
     return {
