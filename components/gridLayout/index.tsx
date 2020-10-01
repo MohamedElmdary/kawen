2e685514d8ee9edac5c3c6214a3b45874982575b
MohamedElmdary <engm5081@gmail.com>
2020-10-01 19:32:05 +0200

    returned to old solution

--- components/gridLayout/index.tsx
+++ components/gridLayout/index.tsx
@@ -1,6 +1,6 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import classes from './gridLayout.module.scss';
-import useMediaQuery from '../../hooks/useMediaQuery';
+import splitArray from '../../shared/utils/splitArray';
 
 interface Props<T> {
     data: T[];
@@ -9,40 +9,19 @@ interface Props<T> {
 }
 
 function GridLayout<T>(props: Props<T>): JSX.Element {
-    const isTablet = useMediaQuery('(max-width: 991px) and (min-width: 767px)');
-    const isMobile = useMediaQuery('(max-width: 768px)');
-    const { data, Component, id } = props;
-    const length = data.length;
-    const refs = Array.from({ length }, () => useRef<HTMLDivElement>(null));
-
-    const n = isTablet ? 2 : 3;
-    const translates: number[] = [];
-    const grid = data.map((item, idx) => {
-        let translateY = 0;
-        if (!isMobile && idx > n - 1) {
-            const ref = refs[idx - n].current;
-            const current = refs[idx].current;
-            if (ref && current) {
-                const { offsetTop, offsetHeight } = ref;
-                translateY =
-                    current.offsetTop -
-                    offsetTop -
-                    offsetHeight +
-                    translates[idx - n];
-            }
-        }
-        translates.push(translateY);
-
-        return (
-            <div
-                className={classes.grid__item}
-                key={id(item, idx)}
-                ref={refs[idx]}
-                style={{ transform: `translateY(${-1 * translateY}px)` }}
-            >
-                <div>
+    const { data, Component, id = (_: T, i: number) => i } = props;
+    const array = splitArray(data, 3);
+    const grid = array.map((col, i) => {
+        const items = col.map((item, i) => {
+            return (
+                <div className={classes.grid__item} key={id(item, i)}>
                     <Component {...{ item }} />
                 </div>
+            );
+        });
+        return (
+            <div className={classes.grid__col} key={i}>
+                {items}
             </div>
         );
     });
@@ -54,31 +33,5 @@ function GridLayout<T>(props: Props<T>): JSX.Element {
         </div>
     );
 }
-
-// function GridLayout<T>(props: Props<T>): JSX.Element {
-//     const { data, Component, id = (_: T, i: number) => i } = props;
-//     const array = splitArray(data, 3);
-//     const grid = array.map((col, i) => {
-//         const items = col.map((item, i) => {
-//             return (
-//                 <div className={classes.grid__item} key={id(item, i)}>
-//                     <Component {...{ item }} />
-//                 </div>
-//             );
-//         });
-//         return (
-//             <div className={classes.grid__col} key={i}>
-//                 {items}
-//             </div>
-//         );
-//     });
-
-//     return (
-//         <div className={classes.grid}>
-//             {/* \n */}
-//             {grid}
-//         </div>
-//     );
-// }
 
 export default GridLayout;
