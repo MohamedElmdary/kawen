3bb1803025e2ba199fa9f9c3fbf29e58b7ebb385
MohamedElmdary <engm5081@gmail.com>
2020-10-05 01:48:42 +0200

    removed un-used type

--- pages/[fieldName]/index.tsx
+++ pages/[fieldName]/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
-import Link from "next/link";
-import Layout from "../../components/layout";
-import classes from "./fieldOverview.module.scss";
+import React from 'react';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+import classes from './fieldOverview.module.scss';
 
 interface Props {
     field: string;
@@ -12,23 +12,23 @@ interface Props {
 }
 
 const data: Props = {
-    field: "Front end development",
+    field: 'Front end development',
     description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, seddo eiusmod tempor incididunt ut labore et dolore magna aliqua',
     field_overview:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magn aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate veli esse cillum dolore eu fugiat nulla pariatur. Excepteur sin occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magn aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate veli esse cillum dolore eu fugiat nulla pariatur. Excepteur sin occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     learning_path: {
         Entry: [
-            "Introduction to HTML",
-            "CSS - the completed course",
-            "CSS3 - Including flexbox, transition, and animation",
+            'Introduction to HTML',
+            'CSS - the completed course',
+            'CSS3 - Including flexbox, transition, and animation',
         ],
         Intermediate: [
-            "Javascript syntax",
-            "Javascript dom",
-            "Javascript events",
+            'Javascript syntax',
+            'Javascript dom',
+            'Javascript events',
         ],
-        Master: ["React.js", "Redux", "Redux saga"],
+        Master: ['React.js', 'Redux', 'Redux saga'],
     },
 };
 
@@ -88,7 +88,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [
             {
-                params: { fieldName: "front" },
+                params: { fieldName: 'front' },
             },
         ],
         fallback: false,
